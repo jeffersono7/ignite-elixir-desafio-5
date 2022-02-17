459f9125ad939f0de566618b616b643f2ab0201a
Jefferson Farias <jefferson.farias7@gmail.com>
2022-02-17 08:43:36 -0300

    config db postgres

--- config/dev.exs
+++ config/dev.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure your database
 config :exmeal, Exmeal.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "exmeal_dev",
+  username: "exmeal",
+  password: "exmeal",
+  database: "exmeal_db",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -6,9 +6,9 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :exmeal, Exmeal.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "exmeal_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: "exmeal",
+  password: "exmeal",
+  database: "exmeal_db#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
